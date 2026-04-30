2df1cbcb88952ef52a738c7cdaf3ef0a5bc5d561
VaruneshT <tvarunesharasu@gmail.com>
2026-04-30 10:18:30 +0530

    Created server.js

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+
+// Routes
+app.use("/api/collect", require("./routes/collect"));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(5000, () =>
+      console.log("Server running on port 5000")
+    );
+  })
+  .catch(err => console.log(err));
+  
